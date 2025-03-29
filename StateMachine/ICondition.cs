471e597fb4112bd8409ce27784884b4d1dd327b9
mio <134259637+nkc20236031@users.noreply.github.com>
2025-03-30 00:19:18 +0900

    Update ICondition.cs

--- StateMachine/ICondition.cs
+++ StateMachine/ICondition.cs
@@ -3,6 +3,11 @@
 	public interface ICondition
 	{
 		/// <summary>
+		/// 初期化を行う
+		/// </summary>
+		public void Initialize();
+		
+		/// <summary>
 		/// 判定を行う
 		/// </summary>
 		public bool Decide();
